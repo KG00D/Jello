51a9330477b38180384b10911f32df7b30100432
KYLE GOOD <kwgood@gmail.com>
2023-12-11 19:39:58 -0500

    Placeholder 2, didnt save

--- app/models/lists.py
+++ app/models/lists.py
@@ -1,14 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-db = SQLAlchemy()
+#TODO Check this model?
+from .board_model import db, Board 
 
 class List(db.Model):
     __tablename__ = 'lists'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
-    board_id = db.Column(db.Integer, db.ForeignKey('boards.id'))
+    boardid = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
+    def __init__(self, title, board):
+        self.title = title
+        self.board = board
 
