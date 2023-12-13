f2076fd3a1a3c3c1d07d80ddf812bc21aee3b59b
Alex Alfadel <alexalfadel@gmail.com>
2023-12-13 15:53:07 -0800

    card form started

---
+++ app/forms/card_form.py
@@ -0,0 +1,9 @@
+##integer
+##String
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired
+
+class CardForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    description = StringField('Description', validators[DataRequired()])
