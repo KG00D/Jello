1def5a91903b4d5dac0e99e85a25266f96db0443
Matthew <matthewhung.shop@gmail.com>
2023-12-13 20:09:08 -0500

    board route / updated

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, request
-from app.models import User, db
-from app.forms import LoginForm
-from app.forms import SignUpForm
+from app.models import Board
 from flask_login import current_user, login_user, logout_user, login_required
 
 board_routes = Blueprint('auth', __name__)
+
+@board_routes.route('/')
+def landing_page():
+    public_boards = Board.query.filter(Board.is_public == True).all()
+    user_boards = []
+    if current_user.is_authenticated:
+        user_query = Board.query.filter(current_user.id)
+
+    return {
+        "public_boards": public_boards,
+        "user_boards": user_boards
+    }
