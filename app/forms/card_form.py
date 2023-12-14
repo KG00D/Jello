46d966855a9decdef9433490b363e5b3117ece42
Alex Alfadel <alexalfadel@gmail.com>
2023-12-13 16:16:31 -0800

    list id field added to card form

--- app/forms/card_form.py
+++ app/forms/card_form.py
@@ -5,4 +5,5 @@ from wtforms.validators import DataRequired, Length
 class CardForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=64)])
     description = StringField('Description', validators[DataRequired(), Length(max=1000)])
+    list_id = IntegerField('List Id', validators=[DataRequired()])
     submit = SubmitField('Submit')
