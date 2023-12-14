4d6e99a10e7474912150d3f611ac359c334f4b36
Matthew <matthewhung.shop@gmail.com>
2023-12-13 20:43:16 -0500

    board route basic

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -6,12 +6,15 @@ board_routes = Blueprint('boards', __name__)
 
 @board_routes.route('/')
 def landing_page():
-    public_boards = Board.query.filter(Board.is_public == True).all()
-    user_boards = []
-    if current_user.is_authenticated:
-        user_query = Board.query.filter(current_user.id).all()
+    public_query = Board.query.all()
+    public_boards = [board.to_dict() for board in public_query]
+    print(public_boards)
+    # user_query = []
+    # if current_user.is_authenticated:
+    #     user_boards = list(Board.query.filter(current_user.id).all())
+
 
     return {
-        "public_boards": public_boards,
-        "user_boards": user_boards
+        "public_boards": public_boards
+        # "user_boards": user_boards
     }
