d80face7df28d6a5c76be14fb0e6bc2b0811acfa
Alex Alfadel <alexalfadel@gmail.com>
2023-12-13 16:17:54 -0800

    user id and card id fields added to comments form

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -4,4 +4,6 @@ from wtforms.validators import DataRequired, Length
 
 class CommentForm(FlaskForm):
     comment_text = StringField('Comment', validators=[DataRequired(), Length(max=1000)])
+    user_id = IntegerField('User Id', validators=[DataRequired()])
+    card_id = IntegerField('Card Id', validators=[DataRequired()])
     submit = SubmitField('Submit')
