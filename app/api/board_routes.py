a0a676e415d662fc5b89404c6099054254f66a08
Matthew <matthewhung.shop@gmail.com>
2023-12-13 20:18:36 -0500

    starting board route

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -2,14 +2,14 @@ from flask import Blueprint, request
 from app.models import Board
 from flask_login import current_user, login_user, logout_user, login_required
 
-board_routes = Blueprint('auth', __name__)
+board_routes = Blueprint('boards', __name__)
 
 @board_routes.route('/')
 def landing_page():
     public_boards = Board.query.filter(Board.is_public == True).all()
     user_boards = []
     if current_user.is_authenticated:
-        user_query = Board.query.filter(current_user.id)
+        user_query = Board.query.filter(current_user.id).all()
 
     return {
         "public_boards": public_boards,
