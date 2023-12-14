c5137bb3c4d46338a3fa8949f1f035bba6509162
Matthew <matthewhung.shop@gmail.com>
2023-12-13 19:46:11 -0500

    starting board routes

---
+++ app/api/board_routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, request
+from app.models import User, db
+from app.forms import LoginForm
+from app.forms import SignUpForm
+from flask_login import current_user, login_user, logout_user, login_required
+
+board_routes = Blueprint('auth', __name__)
