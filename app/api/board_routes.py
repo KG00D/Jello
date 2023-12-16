a5a02b4e97bcd9279b3e416accce73f73a49af7f
Matthew <matthewhung.shop@gmail.com>
2023-12-16 15:29:28 -0500

    create board completed

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, request
-from app.models import Board, List, Card
+from app.models import Board, List, Card, db
 from flask_login import current_user, login_user, logout_user, login_required
+from app.forms.board_form import BoardForm
 
 board_routes = Blueprint('boards', __name__)
 
+# Gets
 @board_routes.route('/')
-# @login_required
 def public_boards():
     public_query = Board.query.filter(Board.is_public==True).all()
     public_boards = [board.to_dict() for board in public_query]
@@ -38,3 +39,26 @@ def board_details(id):
     return {
         "Board Details": board_details
     }
+
+# post
+@board_routes.route('/', methods=["POST"])
+@login_required
+def new_board():
+    form = BoardForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_board = Board(
+            user_id = current_user.id,
+            name = form.data["name"],
+            background_image = form.data["background_image"],
+            # note has to be lowercase coming from the frontend
+            is_public = form.data["is_public"]
+        )
+        db.session.add(new_board)
+        db.session.commit()
+        return new_board.to_dict()
+    return form.errors, 401
+
+# update
+
+# delete
--- app/forms/board_form.py
+++ app/forms/board_form.py
@@ -6,4 +6,3 @@ class BoardForm(FlaskForm):
     name = StringField('name', validators=[DataRequired(), Length(max=64)])
     is_public = BooleanField('is_public')
     background_image = StringField("background_image", validators=[DataRequired(), Length(max=255)])
-    user_id = IntegerField("user_id", validators=[DataRequired()])
