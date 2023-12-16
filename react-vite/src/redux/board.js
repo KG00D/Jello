5b25b19fbe2848d2c4ab1ac9410f84b708f012ef
Matthew <matthewhung.shop@gmail.com>
2023-12-16 18:01:41 -0500

    started front end

---
+++ react-vite/src/redux/board.js
@@ -0,0 +1,12 @@
+const GET_PUBLIC_BOARDS = "board/public";
+const GET_MY_BOARDS = "board/my";
+const GET_BOARD_DETAILS = "board/details";
+const ADD_BOARD = "board/add";
+const EDIT_BOARD = "board/edit";
+const DELETE_bOARD = "board/delete";
+
+const initialState = { publicBoards: null, myBoards: null, boardDetails: null };
+
+function boardReducer(state = initialState, action) {}
+
+export default boardReducer;
