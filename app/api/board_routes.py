2b16a058846f8573cafb04fe2d5bbd3faf4e527c
Matthew <matthewhung.shop@gmail.com>
2023-12-16 13:17:06 -0500

    landing page completed

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -5,16 +5,15 @@ from flask_login import current_user, login_user, logout_user, login_required
 board_routes = Blueprint('boards', __name__)
 
 @board_routes.route('/')
+# @login_required
 def landing_page():
     public_query = Board.query.filter(Board.is_public==True).all()
     public_boards = [board.to_dict() for board in public_query]
-    print(public_boards)
-    # user_query = []
-    # if current_user.is_authenticated:
-    #     user_boards = list(Board.query.filter(current_user.id).all())
-
-
+    my_boards = []
+    if current_user.is_authenticated:
+        my_query = Board.query.filter(Board.user_id == current_user.id).all()
+        my_boards = [board.to_dict() for board in my_query]
     return {
-        "public_boards": public_boards
-        # "user_boards": user_boards
+        "Public Boards": public_boards,
+        "My Boards": my_boards
     }
