f3cad5dc64b701ecd8c3a4629c4b1bb687787f78
Matthew <matthewhung.shop@gmail.com>
2023-12-18 05:36:42 -0500

    added board+ button

---
+++ react-vite/src/components/CreateBoardModal/CreateBoardModal.jsx
@@ -0,0 +1,9 @@
+import "./CreateBoardModal.css";
+import { useModal } from "../../context/Modal";
+
+function CreateBoardModal() {
+
+  return <div className="create-board">New Board</div>;
+}
+
+export default CreateBoardModal;
