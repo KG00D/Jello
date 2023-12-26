257430ffab56c40c6d245c9d6d7c6ad50e00828e
Matthew <matthewhung.shop@gmail.com>
2023-12-26 07:57:41 -0500

    added board options menu

--- react-vite/src/components/SidePanel/SideItem.jsx
+++ react-vite/src/components/SidePanel/SideItem.jsx
@@ -1,7 +1,29 @@
 import { NavLink } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import BoardOptions from "./BoardOptions";
 
 function SideItem({ item }) {
-  console.log(item);
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setShowMenu(!showMenu);
+  };
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu, true);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
   return (
     <li className="side-item">
       <NavLink to={`/boards/${item.id}`}>
@@ -14,10 +36,15 @@ function SideItem({ item }) {
           </div>
           {item.name}
         </div>
-        <span className="item-ellipsis">
+        <span className="item-ellipsis" onClick={toggleMenu}>
           <i class="fa-solid fa-ellipsis"></i>
         </span>
       </NavLink>
+      {showMenu && (
+        <span id={`panel-board-${item.id}`} ref={ulRef}>
+          <BoardOptions boardId={item.id} />
+        </span>
+      )}
     </li>
   );
 }
