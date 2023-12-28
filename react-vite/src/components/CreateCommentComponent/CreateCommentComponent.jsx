a22ea8c05fadf73498de696e7d25a89f945374fc
Kyle Mooney <kjmooney27@gmail.com>
2023-12-28 12:28:53 -0500

    create comment styling

--- react-vite/src/components/CreateCommentComponent/CreateCommentComponent.jsx
+++ react-vite/src/components/CreateCommentComponent/CreateCommentComponent.jsx
@@ -47,21 +47,27 @@ const CreateComment = () => {
 
   return (
     <div className="create-comment-container">
+
       <div className="create-comment-initials">{initials}</div>
+
       <form onSubmit={handleSubmit}>
-        <div>
-          <textarea
-            id= "comment-text"
-            className="create-comment-text"
-            type="text"
-            onChange={e => setCommentText(e.target.value)}
-            onClick={() => setSelected(true)}
-            value={commentText}
-            placeholder="Write a comment..."
-          />
+        <div className="create-comment-form">
+          <div className="create-comment-text-container">
+            <textarea
+              id= "comment-text"
+              className="create-comment-text"
+              type="text"
+              onChange={e => setCommentText(e.target.value)}
+              onClick={() => setSelected(true)}
+              value={commentText}
+              placeholder="Write a comment..."
+              contentEditable
+            />
+          </div>
           <button className={buttonClass}>Save</button>
         </div>
       </form>
+
     </div>
   )
 }
