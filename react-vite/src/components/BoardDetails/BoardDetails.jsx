a5c7ea508c269fe0068f2381d698929081498b21
Kyle <33107816+KG00D@users.noreply.github.com>
2023-12-29 18:55:01 -0500

    Kg lists styling functions (#99)
    * Adding lots of lists stuff
    
    * Fixing weird bug
    
    * Adding rough card styling so we can visualize things better

--- react-vite/src/components/BoardDetails/BoardDetails.jsx
+++ react-vite/src/components/BoardDetails/BoardDetails.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import { useModal } from "../../context/Modal";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { boardDetailsThunk, editBoardThunk } from "../../redux/board";
-import ListCreateModal from "../ListCreateModal";
-import SidePanel from "../SidePanel";
+import { deleteListThunk, updateListTitleThunk } from '../../redux/lists'; // Add these imports
 import DeleteBoardModal from "./DeleteBoardModal";
+import ListEditModal from "../ListEditModal";
+import SidePanel from "../SidePanel";
 import AddCard from "../AddCardComponent/AddCard";
 import Cards from "../CardsComponent/CardsComponent";
-
-
 
 import "./BoardDetails.css";
 
@@ -18,10 +17,14 @@ function BoardDetails() {
   const dispatch = useDispatch();
   const { id } = useParams();
   const boardDetails = useSelector((state) => state.boards.boardDetails[id]);
-  const boards = useSelector((state)=>state.boards)
   const [boardName, setBoardName] = useState("");
   const [isPublic, setIsPublic] = useState(false);
   const [backgroundImage, setBackgroundImage] = useState("");
+
+  const [editingListId, setEditingListId] = useState(null);
+  const [editingTitle, setEditingTitle] = useState("");
+  const [openMenuId, setOpenMenuId] = useState(null);
+
 
   useEffect(() => {
     dispatch(boardDetailsThunk(id));
@@ -50,6 +53,51 @@ function BoardDetails() {
     dispatch(editBoardThunk(boardDetails, id));
   };
 
+  const handleDeleteList = async (listId) => {
+    try {
+      await dispatch(deleteListThunk(id, listId));
+      dispatch(boardDetailsThunk(id));
+    } catch (error) {
+      console.error("Failed to delete list:", error);
+    }
+  };
+  
+  const handleEditListTitle = (list) => {
+    setEditingListId(list.id);
+    setEditingTitle(list.title);
+  };
+
+  const handleSaveTitle = async (listId) => {
+    try {
+      if (editingTitle !== "") {
+        await dispatch(updateListTitleThunk(id, listId, editingTitle));
+        setEditingTitle(""); 
+        dispatch(boardDetailsThunk(id)); 
+      }
+    } catch (error) {
+      console.error("Failed to save title:", error);
+    } finally {
+      setEditingListId(null);
+    }
+  };
+
+  const isMenuOpen = (listId) => {
+    return openMenuId === listId;
+  };
+
+  const toggleMenu = (listId) => {
+    if (openMenuId === listId) {
+      setOpenMenuId(null); 
+    } else {
+      setOpenMenuId(listId); 
+    }
+  };
+  
+  const handleTitleChange = (e) => {
+    setEditingTitle(e.target.value);
+    dispatch(boardDetailsThunk(id)); 
+  };
+
   const deleteBoard = async (e) => {
     e.preventDefault();
     setModalContent(<DeleteBoardModal id={id} />);
@@ -59,14 +107,11 @@ function BoardDetails() {
 
   let Lists = {};
   if (boardDetails.Lists) Lists = { ...boardDetails.Lists };
+  
   return (
-
     <div className="Side-Panel">
       <SidePanel />
-      <div
-        className="board-details"
-        style={{ backgroundColor: background_image }}
-      >
+      <div className="board-details" style={{ backgroundColor: backgroundImage }}>
         <h4>
           Board Name:{" "}
           <input
@@ -77,9 +122,33 @@ function BoardDetails() {
           />
         </h4>
         <div className="lists-container">
-          {Object.values(Lists).map((list) => (
-            <div className="list-container">
-              <h4>List: {list.title}</h4>
+          {Object.values(boardDetails.Lists || {}).map((list) => (
+            <div className="list-container" key={list.id}>
+              <button className="menu-button" onClick={() => toggleMenu(list.id)}>...</button>  
+              {/* Dropdown menu */}
+              {isMenuOpen(list.id) && (
+                <div className="lists-dropdown-menu">
+                  <button className="delete-list-button" onClick={() => handleDeleteList(list.id)}>Delete List</button>
+                  {/* Add more menu items here if we want */}
+                </div>
+              )}
+  
+              {editingListId === list.id ? (
+                <input
+                  type="text"
+                  value={editingTitle}
+                  onChange={handleTitleChange}
+                  onBlur={() => handleSaveTitle(list.id)}
+                  autoFocus
+                  className="edit-list-title-input"
+                />
+              ) : (
+                <h4 onClick={() => handleEditListTitle(list)}>
+                  {list.title}
+                </h4>
+              )}
+  
+              {/* List Cards */}
               {list.Cards &&
                 Object.values(list.Cards).map(
                   (
@@ -103,10 +172,9 @@ function BoardDetails() {
           ))}
         </div>
         <div className="modal-container">
-          <ListCreateModal boardId={id} />
+          <ListEditModal boardId={id} />
         </div>
       </div>
-
     </div>
   );
 }
